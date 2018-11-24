e95d63dc96ae89e3107ee35b16776a6ece5ec664
s1713544 <44524683+s1713544@users.noreply.github.com>
2018-11-24 14:52:04 +0000

    Added javadoc comments

--- src/auctionhouse/Money.java
+++ src/auctionhouse/Money.java
@@ -6,11 +6,20 @@ package auctionhouse;
 /**
  * @author pbj
  */
+
+/**
+* <h1>Money implementation</h1>
+* This class implements all necessary methods for money transactions in the course work.
+* The Money class also implements the comparable interface which allows for comparisons between instances of the Money class.
+*/
+
 public class Money implements Comparable<Money> {
  
-    private double value;
+	/** Represents the monetary value of the particular instance of Money.
+	*/
+	private double value;
     
-    private static long getNearestPence(double pounds) {
+	private static long getNearestPence(double pounds) {
         return Math.round(pounds * 100.0);
     }
  
@@ -27,14 +36,26 @@ public class Money implements Comparable<Money> {
         value = pounds;
     }
     
+    /** Adds the value of one instance of Money to another.
+	 * @param m The instance of Money to be added to the current instance of Money.
+	 * @return The current instance of Money to which the value of the other instance of Money is added to.
+	*/
     public Money add(Money m) {
         return new Money(value + m.value);
     }
     
+    /** Subtracts the value of one instance of Money from the another.
+	 * @param m The instance of Money to be subtracted from the current instance of Money.
+	 * @return The current instance of Money to which the value of the other instance of Money has been subtracted from.
+	*/
     public Money subtract(Money m) {
         return new Money(value - m.value);
     }
  
+    /** Adds a percentage of the current instance of Money's value to itself..
+	 * @param percent A double that entails the percentage of Money's value to be added to itself.
+	 * @return The current instance of Money but with a larger value field.
+	*/
     public Money addPercent(double percent) {
         return new Money(normalise(value * (1 + percent/100.0)));
     }
@@ -44,14 +65,27 @@ public class Money implements Comparable<Money> {
         return String.format("%.2f", value);
         
     }
+    /** Compares the values of two different instances of Money.
+	 * @param m The instance of Money to be compared to the current instance of Money.
+	 * @return 1 if the current instance of Money' value is larger than m's value, 0 if they are equal and -1 if m's value is larger than the current instance value.
+	*/
     public int compareTo(Money m) {
         return Long.compare(getNearestPence(value),  getNearestPence(m.value)); 
     }
     
+    /** Tests whether the value of the current instance of Money is less than or equal to the value of some supplied instance of Money.
+	 * @param m The instance of Money to be compared to the current instance of Money.
+	 * @return 1 if the current instance of Money's value is smaller or equal to m's value.
+	*/
     public Boolean lessEqual(Money m) {
         return compareTo(m) <= 0;
     }
     
+    /** Tests whether the value of the current instance of Money equal to another instance of Money.
+     * ALso overrides the "equals" method of the comparable interface.
+	 * @param o The object (instance of Money) to be compared to the current object.
+	 * @return 1 if the instances of the Money object are equal.
+	*/
     @Override
     public boolean equals(Object o) {
         if (!(o instanceof Money)) return false;
